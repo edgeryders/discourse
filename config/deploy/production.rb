682a92b5ca8df7487608a9aa5e51271f49105219
damingo <daniel@ansorgs.de>
2019-04-08 20:42:53 +0200

    Update trustinplay domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
-
 server 'edgeryders.eu', user: 'discourse', roles: [:web, :app, :db], primary: true
 
 set :puma_bind, %w(tcp://0.0.0.0:9292 unix:///home/discourse/production/current/tmp/sockets/puma.sock)
