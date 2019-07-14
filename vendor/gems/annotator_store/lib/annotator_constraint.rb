7e2fc83c53f47d6966637e0251f7b20cba54b278
damingo <daniel@ansorgs.de>
2019-07-14 16:28:28 +0200

    Fixes #195

--- vendor/gems/annotator_store/lib/annotator_constraint.rb
+++ vendor/gems/annotator_store/lib/annotator_constraint.rb
@@ -5,7 +5,7 @@ class AnnotatorConstraint
   def matches?(request)
     provider = Discourse.current_user_provider.new(request.env)
     provider.current_user &&
-      (provider.current_user.staff? || provider.current_user.groups.pluck(:name).map(&:downcase).include?('annotator')) &&
+      (provider.current_user.admin? || provider.current_user.groups.pluck(:name).map(&:downcase).include?('annotator')) &&
       custom_staff_check(request)
   rescue Discourse::InvalidAccess, Discourse::ReadOnly
     false
