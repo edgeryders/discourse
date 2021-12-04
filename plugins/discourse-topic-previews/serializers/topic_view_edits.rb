ca417218c0dad85c7a387f4ebe835d1d322ec228
Daniel <macbook@MBP.fritz.box>
2021-12-04 23:57:08 +0100

    Update Discourse to 2.7.11

--- plugins/discourse-topic-previews/serializers/topic_view_edits.rb
+++
@@ -1,9 +0,0 @@
-require 'topic_view_serializer'
-class ::TopicViewSerializer
-
-  attributes :user_chosen_thumbnail_url
-
-  def user_chosen_thumbnail_url
-    object.topic.custom_fields['user_chosen_thumbnail_url']
-  end
-end
