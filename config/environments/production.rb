81d2f7aeec8f3285c350b5e3562566edc36d9ba4
Daniel <daniel@ansorgs.de>
2023-01-20 19:26:22 +0100

    Test db pool setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Discourse::Application.configure do
 
   config.log_level = :info
 
+  config.pool = 30
+
   if GlobalSetting.smtp_address
     settings = {
       address: GlobalSetting.smtp_address,
